94bd0fafd64c15c12c78aa793d1c10b20c011df1
James <james1239090@gmail.com>
2017-07-22 17:21:09 +0800

    add the index of spec

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
+  describe 'Get Index' do
+    it 'assigns @courses and render template' do
+      course1 = Course.create(title: "foo", description: "bar")
+      course2 = Course.create(title: "bar", description: "foo")
 
+      get :index
+
+      expect(assigns[:courses]).to eq([course1, course2])
+      expect(respones).to render_template("index")
+    end
+  end
 end
